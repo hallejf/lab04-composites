9dabe26497d02915712a9321c856aa1fb67d903f
Halle Friedland <hallefriedland@D-108-179-146-210.dhcp4.washington.edu>
2017-01-24 11:18:42 -0800

    did part

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,32 @@ interface IDomElement {
   print();
 }
 
+class DomElement implements IDomElement {
+  elementType:string;
+  storeInfo:IDomElement[]; 
+
+  constructor(elementType:string) {
+    this.elementType = elementType; 
+    this.storeInfo = [];
+  }
+
+  add(information : IDomElement) {
+    this.storeInfo.push(information); 
+  }
+
+  print() {
+
+  }
+}
+
+class TextNode implements IDomElement {
+  private content:string;
+  
+  constructor(content:string) {
+    this.content = content;
+  }
+
+  print() {
+    console.log(this.content); 
+  }
+}
